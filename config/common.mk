a17428891528b2f6ffbb1a7953ec00ffea83c6c1
COSMIC <rayavanindra@gmail.com>
2024-06-29 11:24:56 +0530

    vendor: Add SystemUI clocks

--- config/common.mk
+++ config/common.mk
@@ -128,6 +128,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     build-manifest
 
+# Clocks
+PRODUCT_PACKAGES += \
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Flex \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-Metro \
+    SystemUIClocks-Weather
+
 # Lineage packages
 ifeq ($(PRODUCT_IS_ATV),)
 PRODUCT_PACKAGES += \
